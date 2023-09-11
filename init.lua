1e96264cb1b147dc7d254c5bb609ef6fa8b3c0a7
Talha Qamar <qamartalha@protonmail.com>
2023-09-11 10:33:41 +0500

    configured orgmode

--- init.lua
+++ init.lua
@@ -226,8 +226,27 @@ require('lazy').setup({
   {
     'nvim-orgmode/orgmode',
     config = function()
-      require('orgmode').setup{}
-    end
+      -- init.lua
+      require('orgmode').setup_ts_grammar()
+      -- Treesitter configuration
+      require('nvim-treesitter.configs').setup {
+        -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+        -- highlighting will fallback to default Vim syntax highlighting
+        highlight = {
+          enable = true,
+          -- Required for spellcheck, some LaTex highlights and
+          -- code block highlights that do not have ts grammar
+          additional_vim_regex_highlighting = {'org'},
+        },
+        ensure_installed = {'org'}, -- Or run :TSUpdate org
+      }
+
+      require('orgmode').setup({
+        org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
+        org_default_notes_file = '~/Dropbox/org/refile.org',
+      })
+    end,
+    dependencies = {"nvim-treesitter/nvim-treesitter"}
   }
 
 
@@ -529,6 +548,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    {name = 'orgmode'},
   },
 }
 
